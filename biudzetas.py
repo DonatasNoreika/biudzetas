702363a02bafaf9367c790a96304af61f21a0b1a
DonatasNoreika <donoras@gmail.com>
2024-07-11 18:40:41 +0300

    Atnaujinta versija su pickle

--- biudzetas.py
+++ biudzetas.py
@@ -1,19 +1,34 @@
 from models.pajamu_irasas import PajamuIrasas
 from models.islaidu_irasas import IslaiduIrasas
+import pickle
 
 class Biudzetas:
     def __init__(self):
-        self.zurnalas = []
+        self.zurnalas = self.nuskaityti_is_failo()
+
+    def nuskaityti_is_failo(self):
+        try:
+            with open("zurnalas.pkl", 'rb') as file:
+                zurnalas = pickle.load(file)
+        except:
+            zurnalas = []
+        return zurnalas
+
+    def irasyti_i_faila(self):
+        with open("zurnalas.pkl", 'wb') as file:
+            pickle.dump(self.zurnalas, file)
 
     def prideti_pajamas(self):
         suma = abs(float(input("Suma: ")))
         irasas = PajamuIrasas(suma)
         self.zurnalas.append(irasas)
+        self.irasyti_i_faila()
 
     def prideti_islaidas(self):
         suma = abs(float(input("Suma: ")))
         irasas = IslaiduIrasas(suma)
         self.zurnalas.append(irasas)
+        self.irasyti_i_faila()
 
     def atspaudinti_zurnala(self):
         print("------------------------")
